3bc9e457f38b7295ef0c49e6ea4123b8de0ecc17
Salikh <pernebek.sm@gmail.com>
2023-07-24 14:20:27 +0600

    vue 3 added to the project

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,25 +1,36 @@
 <div>
-    <table class="table">
-        <tr class="table-primary">
-            @foreach( $columns as $column )
-                <th>{{ $column }}</th>
-            @endforeach
-            <th>actions</th>
-        </tr>
-        @foreach($values as $row)
-            <tr>
-                @foreach($row as $cell)
-                    <td> {{ $cell }} </td>
+    <div class="table-header">
+        <div class="d-flex justify-content-end m-2">
+            <button class="btn btn-success">
+                <i class="fa-solid fa-circle-plus me-2"></i>
+                add
+            </button>
+        </div>
+    </div>
+    <div class="table-body">
+        <table class="table">
+            <tr class="table-primary">
+                @foreach( $columns as $column )
+                    <th>{{ $column }}</th>
                 @endforeach
-                <td>
-                    <button class="btn btn-sm btn-warning rounded-circle">
-                        <i class="fa-solid fa-pencil"></i>
-                    </button>
-                    <button class="btn btn-sm btn-danger rounded-circle">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </td>
+                <th>actions</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach($values as $row)
+                <tr>
+                    @foreach($row as $cell)
+                        <td> {{ $cell }} </td>
+                    @endforeach
+                    <td>
+                        <button class="btn btn-sm btn-warning rounded-circle">
+                            <i class="fa-solid fa-pencil"></i>
+                        </button>
+                        <button class="btn btn-sm btn-danger rounded-circle">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+    <div class="table-footer"></div>
 </div>
--- resources/views/tasks/todo.blade.php
+++ resources/views/tasks/todo.blade.php
@@ -2,12 +2,7 @@
 
 @section('content')
     <div class="table-section container">
-        <div class="d-flex justify-content-end m-2">
-            <button class="btn btn-success">
-                <i class="fa-solid fa-circle-plus me-2"></i>
-                Add Task
-            </button>
-        </div>
         <x-table :columns="$columns" :values="$values"/>
     </div>
+
 @endsection
